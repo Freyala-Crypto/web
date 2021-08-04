1215a8896ea138858151e8d923e105732864018b
Tailchakra <chrisvdgeld@gmail.com>
2021-08-04 16:13:56 +0200

    Fixed faucet

--- src/pages/Faucet.vue
+++ src/pages/Faucet.vue
@@ -13,84 +13,108 @@
       <div class="container mx-auto py-12 lg:py-24 text-center">
         <div class="w-full">
           <p class="text-2xl lg:text-5xl font-extralight">
-<!--            Get your daily <span class="font-semibold text-primary-alt">free dose of XYA</span> here!-->
-            Temporarily <span class="font-semibold text-primary-alt">out of order</span>!
+            Get your daily <span class="font-semibold text-primary-alt">free dose of XYA</span> here!
           </p>
         </div>
       </div>
     </section>
 
-<!--    <section id="section-i-3">-->
-<!--      <div class="container w-full lg:w-3/4 xl:w-1/2 mx-auto pb-24">-->
-<!--        <input id="input" placeholder="ONE Address" type="text" class="px-4 w-full py-2 text-2xl text-black border-2 border-primary-alt" />-->
-
-<!--        <br>-->
-<!--        <br>-->
-
-<!--        <div class="flex">-->
-<!--          <p id="loading" class="text-xl lg:text-2xl mb-4 text-black">Queuing...</p>-->
-<!--          <p id="output" class="text-xl lg:text-2xl mb-4 text-black"></p>-->
-<!--        </div>-->
-
-<!--        <div class="flex flex-wrap">-->
-<!--          <div class="w-full lg:w-1/2 mb-8 lg:mb-0">-->
-<!--            <div id="recaptcha" class="g-recaptcha" data-callback="recaptchaCallback" data-sitekey="6LfRZQobAAAAAN5hj1f9Y3l5k1u_whZaAOlbWKp8"></div>-->
-<!--          </div>-->
-<!--          <div class="w-full lg:w-1/2 text-right">-->
-<!--            <button id="submit" class="tracking-widest uppercase bg-gradient-to-r from-primary to-secondary py-4 px-8 rounded-md text-white text-xl font-semibold">-->
-<!--              Finish your daily quest!-->
-<!--            </button>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </section>-->
+    <section id="section-i-3">
+      <div class="container w-full lg:w-3/4 xl:w-1/2 mx-auto pb-24">
+        <div class="flex flex-wrap">
+          <div class="w-full text-xl font-bold mb-8 text-center">
+            <p>
+              {{ error }}
+            </p>
+            <p>
+              {{ msg }}
+            </p>
+          </div>
+          <div v-if="msg === ''" class="w-full text-center">
+            <button @click="requestFromFaucet" id="submit" class="tracking-widest uppercase bg-gradient-to-r from-primary to-secondary py-4 px-8 rounded-md text-white text-xl font-semibold">
+              Finish your daily quest!
+            </button>
+          </div>
+        </div><div class="w-full text-center">
+        <small>
+          <em>The soup kitchen will send you 0.75 XYA on request.</em>
+        </small>
+      </div>
+      </div>
+    </section>
 
     <looking-for-these-component/>
-
-    <script type="application/javascript" src="https://www.google.com/recaptcha/api.js" async defer></script>
   </main>
 </template>
 
 <script>
 import LookingForTheseComponent from '@/components/LookingForThese'
+import FreyalaFaucetAbi from "../plugins/faucetArtifact.json";
+import { initWeb3 } from "@/plugins/initWeb3";
 
 export default {
   name: 'Faucet',
   components: {
     LookingForTheseComponent
   },
+  data() {
+    return {
+      input: '',
+      accounts: [],
+      MMerror: '',
+      msg: '',
+      error: '',
+      loading: false,
+      gasPrice: 0,
+      gasLimit: 150000
+    }
+  },
+  methods: {
+    async requestFromFaucet() {
+      this.loading = true
 
-  mounted() {
-    const qs = (x) => document.querySelector(x)
-
-    qs('#submit').disabled = 'true'
-    qs('#loading').style.display = 'none'
-    window.recaptchaCallback = function recaptchaCallback(token) {
-      qs('#submit').removeAttribute('disabled')
-      qs('#submit').onclick = () => {
-        qs('#output').innerHTML = ''
-        qs('#loading').style.display = 'block'
-        const address = qs('#input').value
-        fetch('https://faucet-api.freyala.com/fund', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            address,
-            token
-          })
-        })
-            .then((res) => res.json())
-            .then((res) => {
-              qs('#loading').style.display = 'none'
-              if (res.success === true) {
-                qs('#output').innerHTML = '<h4>Queued!</h4>' + res.message
-              } else {
-                qs('#output').innerHTML = '<h4>An error occurred!</h4>' + res.message
-              }
-            })
+      let web3
+      try {
+        web3 = await initWeb3();
+      } catch (err) {
+        this.MMError = 'test';
+        this.loading = false;
+        return;
       }
+
+      if (web3 === 'No MetaMask installed.') {
+        this.MMError = 'No MetaMask installed.';
+        return;
+      }
+
+      this.accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+      if (networkId !== 1666600000) {
+        this.MMError = "Please connect to the Harmony Mainnet";
+        this.loading = false;
+        return;
+      }
+
+      this.gasPrice = await web3.eth.getGasPrice();
+      this.freyalaFaucet = new web3.eth.Contract(FreyalaFaucetAbi.abi, FreyalaFaucetAbi.address);
+
+      const allowedToWithdraw = await this.freyalaFaucet.methods.allowedToWithdraw(this.accounts[0]).call()
+
+      if (allowedToWithdraw) {
+        await this.freyalaFaucet.methods.requestTokens().send({
+          from: this.accounts[0],
+          gasPrice: this.gasPrice,
+          gasLimit: this.gasLimit,
+          gas: parseFloat((this.gasPrice * this.gasLimit) / Math.pow(10, 9))
+        });
+
+        this.msg = 'The kitchen has successfully sent you 0.75 XYA!'
+      } else {
+        this.msg = 'The kitchen has already given you your portion of soup in the past 24 hours. Come back later.'
+      }
+
+
+      this.loading = false;
     }
   }
 }
